adaa21445294ab2c3ec3951c522b68c5b39b4de3
milan <milan@milan.wtf>
2024-07-09 11:14:18 +0200

    feat: anti-firefly coordination

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,11 @@ use esp_hal::{
     prelude::*,
     system::SystemControl,
 };
-use esp_wifi::current_millis;
+use esp_println::println;
+use esp_wifi::{
+    current_millis,
+    esp_now::{EspNow, BROADCAST_ADDRESS},
+};
 
 #[entry]
 fn main() -> ! {
@@ -26,7 +30,7 @@ fn main() -> ! {
     esp_println::logger::init_logger_from_env();
 
     let timer = esp_hal::timer::timg::TimerGroup::new(peripherals.TIMG1, &clocks, None).timer0;
-    let _init = esp_wifi::initialize(
+    let init = esp_wifi::initialize(
         esp_wifi::EspWifiInitFor::Wifi,
         timer,
         esp_hal::rng::Rng::new(peripherals.RNG),
@@ -34,16 +38,38 @@ fn main() -> ! {
         &clocks,
     )
     .unwrap();
+    let wifi = peripherals.WIFI;
+    let mut esp_now = EspNow::new(&init, wifi).unwrap();
 
     let interval = 2000;
     let mut time_slot = current_millis();
+    let mut before = time_slot;
+    let mut after = None;
     loop {
         if current_millis() >= time_slot {
             time_slot = current_millis() + interval;
+            after = None;
+
+            esp_now
+                .send(&BROADCAST_ADDRESS, &[0])
+                .unwrap()
+                .wait()
+                .unwrap();
 
             led.set_high();
             delay.delay(10.millis());
             led.set_low();
+        }
+
+        if esp_now.receive().is_some() {
+            println!("Received ping");
+            if after.is_none() {
+                after = Some(current_millis());
+                let median = (after.unwrap() - before) / 2;
+                time_slot = before + median + interval;
+            } else {
+                before = current_millis();
+            }
         }
     }
 }
